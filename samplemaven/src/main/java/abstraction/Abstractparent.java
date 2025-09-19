04e362fe1c591f3c66779620b73e89532058721f
sandeepvarghese7 <sandeepvarghese7@gmail.com>
2025-09-19 10:09:44 +0530

    push

---
+++ samplemaven/src/main/java/abstraction/Abstractparent.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public abstract class Abstractparent {
+	//abstract method
+	
+public abstract void display();
+
+public void print()
+{
+	System.out.println("parent");
+}
+}
